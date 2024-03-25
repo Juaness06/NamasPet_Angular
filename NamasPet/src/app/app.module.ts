4cce23e97fa92af6bdfc3c831ea69adb2f27eafb
Juaness06 <jesteban.granada@javeriana.edu.co>
2024-03-25 17:56:55 -0500

    landing page, sin funcionalidades

--- NamasPet/src/app/app.module.ts
+++ NamasPet/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ListaMascotasComponent } from './mascotas/lista-mascotas/lista-mascotas.component';
+import { VerMascotaComponent } from './mascotas/ver-mascota/ver-mascota.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingPageComponent,
+    ListaMascotasComponent,
+    VerMascotaComponent
   ],
   imports: [
     BrowserModule,
