b23958185fd901df30b7267581b92fee5eb233a8
Nicolas Quintana <140460204+Nikoquin1@users.noreply.github.com>
2024-04-21 16:58:42 -0500

    agregar y editar

--- NamasPet/src/app/administrador/veterinarios/agregar-veterinarios/agregar-veterinarios.component.ts
+++ NamasPet/src/app/administrador/veterinarios/agregar-veterinarios/agregar-veterinarios.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { Veterinario } from 'src/app/model/veterinario';
+import { VeterinarioService } from 'src/app/service/veterinario.service';
 
 @Component({
   selector: 'app-agregar-veterinarios',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./agregar-veterinarios.component.css']
 })
 export class AgregarVeterinariosComponent {
+  constructor(
+    private router: Router,
+    private veterinarioService: VeterinarioService // Nombre corregido a CamelCase
+  ) {}
 
+  @Output()
+  addClienteEvent = new EventEmitter<Veterinario>();
+
+  sendCliente!: Veterinario;
+
+  formVeterinario: Veterinario = {
+    id: 0,
+    nombre: '',
+    especialidad: '',
+    atenciones:0,
+    foto:'',
+    activo:true
+  };
+
+  registrarFormVeterinario() {
+    this.sendCliente = Object.assign({}, this.formVeterinario);
+    this.veterinarioService.agregarVeterinario(this.formVeterinario); // Método corregido para agregar un cliente
+    this.router.navigate(['/veterinarios/all']); // Navegación actualizada al listado de clientes
+  }
+
+  registrarVeterinario(): void {
+    this.sendCliente = Object.assign({}, this.formVeterinario);
+    this.addClienteEvent.emit(this.sendCliente);
+  }
 }
--- NamasPet/src/app/administrador/veterinarios/editar-veterinarios/editar-veterinarios.component.ts
+++ NamasPet/src/app/administrador/veterinarios/editar-veterinarios/editar-veterinarios.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Veterinario } from 'src/app/model/veterinario';
+import { VeterinarioService } from 'src/app/service/veterinario.service';
 
 @Component({
   selector: 'app-editar-veterinarios',
@@ -6,5 +9,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./editar-veterinarios.component.css']
 })
 export class EditarVeterinariosComponent {
+  constructor(
+    private veterinarioService: VeterinarioService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
+  @Output()
+  updateClienteEvent = new EventEmitter<Veterinario>();
+
+  sendVeterinario!: Veterinario;
+  buscarVeterinario!: Veterinario;
+
+  formVeterinario: Veterinario = {
+    id: 0,
+    nombre: '',
+    especialidad: '',
+    atenciones:0,
+    foto:'',
+    activo:true
+  };
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const cedula = Number(params.get('cedula'));
+      console.log('Cédula obtenida de la ruta:', cedula); // Para depuración
+      if (cedula) {
+        this.veterinarioService.findById(cedula).subscribe(
+          (clienteInformacion) => {
+            console.log('Cliente obtenido del servicio:', clienteInformacion); // Para depuración
+            this.formVeterinario = clienteInformacion;
+          },
+          (error) => {
+            console.error('Error al buscar el cliente:', error);
+            // Aquí también puedes manejar la reasignación de `formVeterinario` o simplemente dejar el objeto como estaba.
+          }
+        );
+      }
+    });
+  }
+  
+
+  actualizarCliente() {
+    this.sendVeterinario = Object.assign({}, this.formVeterinario);
+    this.veterinarioService.editarvVeterinario(this.sendVeterinario).subscribe(() => {
+      this.updateClienteEvent.emit(this.sendVeterinario); // Emitir el cliente actualizado
+      this.router.navigate(['/clientes/all']);
+    });
+  }
 }
