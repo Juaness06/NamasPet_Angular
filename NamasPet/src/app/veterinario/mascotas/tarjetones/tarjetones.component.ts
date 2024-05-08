70b2a0d0b4e33e2c4a34b827d51b87351fc19b38
Juaness06 <jesteban.granada@javeriana.edu.co>
2024-05-07 22:53:31 -0500

    search bar

--- NamasPet/src/app/veterinario/mascotas/tarjetones/tarjetones.component.ts
+++ NamasPet/src/app/veterinario/mascotas/tarjetones/tarjetones.component.ts
@@ -11,17 +11,34 @@ import { Router } from '@angular/router';
 export class TarjetonesComponent implements OnInit {
   
   mascotasList: Mascota[] = [];
+  mascotasFiltradas: Mascota[] = [];
   seleccionarMascota?: Mascota;
+  filtro: string = '';
 
   constructor(private mascotaService: MascotaService, private router: Router) {}
 
   ngOnInit(): void {
-    this.mascotaService.findAll().subscribe((mascotas) => (this.mascotasList = mascotas));
-    console.log('Numero de mascotas:' + this.mascotasList.length);
+    this.mascotaService.findAll().subscribe((mascotas) => {
+      this.mascotasList = mascotas;
+      this.mascotasFiltradas = mascotas; // Asegúrate de asignar también a mascotasFiltradas
+      console.log('Numero de mascotas:', this.mascotasList.length);
+    }, error => {
+      console.error('Error al cargar las mascotas:', error);
+    });
   }
 
   mostrarMascota(mascotaId: number): void {
     this.router.navigate(['/mascotas', mascotaId]); // Navega a la página de detalles de la mascota
+  }
+
+  filtrarMascotas(): void {
+    if (this.filtro) {
+      this.mascotasFiltradas = this.mascotasList.filter(mascota =>
+        mascota.nombre.toLowerCase().includes(this.filtro.toLowerCase())
+      );
+    } else {
+      this.mascotasFiltradas = this.mascotasList;
+    }
   }
 
 }
